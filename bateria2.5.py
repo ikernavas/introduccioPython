2f594cbb23ac172ee2d5e9c47a7f4f2da30eabc3
ikernavas <158268504+ikernavas@users.noreply.github.com>
2024-02-05 10:00:14 +0100

    Create bateria2.5.py

---
+++ bateria2.5.py
@@ -0,0 +1,7 @@
+anys_experiencia = int(input("Cuants anys portes treballant com a programador? "))
+
+if anys_experiencia < 5:
+    clasificacio = "Junior"
+else:
+    clasificacio = "Senior"
+print("Ets clasificat com:", clasificacio)
