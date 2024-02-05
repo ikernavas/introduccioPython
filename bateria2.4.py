0819160aca6f6b5c3a6c0fa0cfdb86a6187e7be4
ikernavas <158268504+ikernavas@users.noreply.github.com>
2024-02-05 09:51:32 +0100

    Create bateria2.4.py

---
+++ bateria2.4.py
@@ -0,0 +1,14 @@
+# Programa que pregunta la nota a l'usuari i segons la nota diu el resultat obtingut: 1 a 4 - Insuficient, 5 - Suficient, 6 - Bé, 7 a 8 - Notable, 9 a 10 - Excel·lent.
+
+nota = float(input("Diga'm la teva nota: "))
+if nota >= 1 and nota <= 4:
+    resultat = "Insuficient
+elif nota == 5:
+    resultat = "Suficient
+elif nota == 6:
+    resultat = "Bé"
+elif nota >= 7 and nota <= 8:
+    resultat = "Notable"
+elif nota >= 9 and nota <= 10:
+    resultat = "Excelent"
+print "Tens un", resultat)
