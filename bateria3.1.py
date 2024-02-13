d5a711992e827471c836179552bfd75b9259c5ff
ikernavas <158268504+ikernavas@users.noreply.github.com>
2024-02-13 08:50:29 +0100

    Create bateria3.1.py

---
+++ bateria3.1.py
@@ -0,0 +1,6 @@
+# Fes un programa que demana un número a l'usuari i imprimeix la seva taula de multiplicar (del 1 al 10).
+
+numero = int(input("Introdueix un número: "))
+print(f"Taula de multiplicar del {numero}:")
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
