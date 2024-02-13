8de0f8f001df6e616aa61b018e20735d74993cc3
ikernavas <158268504+ikernavas@users.noreply.github.com>
2024-02-13 08:48:55 +0100

    Update funciofactorial.py

--- funciofactorial.py
+++ funciofactorial.py
@@ -2,15 +2,15 @@
 
 def factorial(n):
     if n < 0:
-        return None  # Retornar None si l'entrada és negativa
+        return None  
     elif n == 0:
-        return 1  # El factorial de 0 és 1
+        return 1  
     else:
         resultat = 1
         for i in range(1, n + 1):
             resultat *= i
         return resultat
 
-# Exemple d'ús
+
 numero = 5
 print(f"El factorial de {numero} és {factorial(numero)}")
